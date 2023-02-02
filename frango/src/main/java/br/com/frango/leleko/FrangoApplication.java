010457fbfcd091a34529078a49eba3537fbb723a
Kaio <kgalvo47@gmail.com>
2023-02-02 20:37:27 -0300

    criação do projeto

---
+++ frango/src/main/java/br/com/frango/leleko/FrangoApplication.java
@@ -0,0 +1,13 @@
+package br.com.frango.leleko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrangoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrangoApplication.class, args);
+	}
+
+}
---
+++ frango/src/test/java/br/com/frango/leleko/FrangoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.frango.leleko;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrangoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
